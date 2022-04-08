0cb60c569e2002d3d1237f949dd414275068bb05
Lucas Shangrita <oustanilokman@gmail.com>
2022-04-08 12:56:45 +0300

    Add functionality for adding and receiving data from and to the API

--- modules/scoresApi.js
+++ modules/scoresApi.js
@@ -1,24 +1,21 @@
-const BASE_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/'
-const gameId = 'V83c8NBjJ3i8eCzKJ7qO'
+const BASE_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+const gameId = 'V83c8NBjJ3i8eCzKJ7qO';
 
-const addScore = async (user, score) => {
-  await fetch(`${BASE_URL}games/${gameId}/scores/`, {
+export const addScore = async (name, score) => {
+  await fetch(`${BASE_URL}${gameId}/scores`, {
     method: 'POST',
-    body: JSON.stringify(
-      {
-        user,
-        score,
-      }
-    ),
-		headers: {
+    body: JSON.stringify({
+      user: name,
+      score,
+    }),
+    headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
-  }).then(res => res.json()).then(data => console.log(data))
-}
+  }).then((res) => res.json());
+};
 
- addScore('fe,i', 59)
-const getScore = async () => {
-  await fetch(`${BASE_URL}games/${gameId}/scores/`).then(res => res.json()).then(data => console.log(data))
-}
-
-getScore()
+export const getScore = async () => {
+  let data;
+  await fetch(`${BASE_URL}${gameId}/scores`).then((res) => res.json()).then((res) => { data = res; });
+  return data;
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import './style.css';
-import addScore from '../modules/scoresApi.js';
+
+import { addScore, getScore } from '../modules/scoresApi.js';
+
+const refreshBtn = document.querySelector('#refresh');
+const form = document.querySelector('.add-scores');
+const scoresList = document.getElementById('score-list');
+const name = document.getElementById('name-value');
+const score = document.getElementById('score-value');
+const loadScores = () => {
+  document.querySelector('.rotate-refresh').style.display = 'inline-block';
+  getScore().then((data) => {
+    const scores = data.result;
+    scoresList.innerHTML = '';
+    scores.forEach((item) => {
+      scoresList.innerHTML += `<li>${item.user}: ${item.score}</li>`;
+    });
+  });
+  setTimeout(() => { document.querySelector('.rotate-refresh').style.display = 'none'; }, 1500);
+};
+
+refreshBtn.addEventListener('click', () => {
+  loadScores();
+});
+
+window.addEventListener('load', () => {
+  loadScores();
+});
+
+form.addEventListener('submit', async (e) => {
+  const success = document.querySelector('.success');
+  e.preventDefault();
+  document.querySelector('.rotate-submit').style.display = 'inline-block';
+  await addScore(name.value, score.value).then(() => {
+    success.textContent = `${name.value}'s score added successfully!`;
+    setTimeout(() => {
+      success.style.display = 'none';
+    }, 2500);
+  });
+  form.reset();
+  setTimeout(() => { document.querySelector('.rotate-submit').style.display = 'none'; }, 1000);
+  loadScores();
+});
 
